0739ddecbff05533db403076c336288c8bff6ec6
sbean-han <123240330+sbean-han@users.noreply.github.com>
2024-10-20 08:54:04 +0000

    web init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import json
+from CheckItOut import checkitoutAPI
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+reviews=checkitoutAPI.get_reviews()
+
+st.title("🎈 책키라웃 리뷰 Summary Page!")
+review_summary=[rv.summary for rv in reviews]
+data=pd.DataFrame(review_summary)
+st.dataframe(data)
